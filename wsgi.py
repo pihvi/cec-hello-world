e99bfa9b8b169aff6dad9b61025f679d7fe36e81
Antti Mattila <antti.mattila@reaktor.fi>
2018-02-23 20:54:37 +0200

    read write log

--- wsgi.py
+++ wsgi.py
@@ -1,12 +1,21 @@
 import socket
+import datetime
 from flask import Flask
+
+logname = "/mnt/podi.log"
+file = open(logname, "a")
+file.write(str(datetime.datetime.now()) + " \t" + socket.gethostname() + "\n")
+file.close()
 
 application = Flask(__name__)
 
+
 @application.route("/")
 def hello():
-    
-    return "Hello World! Greetings from "+socket.gethostname()+"\n"
+    file2 = open(logname, "r")
+    res = file2.readlines()
+    file2.close()
+    return "<br>".join(res)
 
 
 if __name__ == "__main__":
